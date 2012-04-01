3042648c34aa005b99c6bc40a90555c990c5e3e9
elbuo <yamil.asusta@upr.edu>
2012-03-31 20:38:22 -0400

    Added the frame code

--- test1/ShuttleBeep.java
+++ test1/ShuttleBeep.java
@@ -1,12 +1,28 @@
 package test1;
 
+
+import javax.swing.JFrame;
+
 public class ShuttleBeep {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		/*
+		Toolkit toolk = Toolkit.getDefaultToolkit(); 
+		Dimension screenSize = tk.getScreenSize();
+		int screenHeight = (screenSize.height/2);
+		int screenWidth = (screenSize.width/2);
+		*/
+		Logic logic = new Logic();
+		frame.add(logic);
+		frame.addKeyListener(logic);
+		frame.addMouseListener(logic);
+		frame.setVisible(true);
 
 	}
 
