006ce89364198c9bbacb77519e3fc90e3173660d
elbuo <yamil.asusta@upr.edu>
2012-04-02 19:00:02 -0400

    Initial commit. Added basic methods to create the file, and present it in the screen.

---
+++ test1/HighScores.java
@@ -0,0 +1,50 @@
+package test1;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class HighScores {
+
+	public HighScores() {
+
+	}
+
+	public void generateFile() throws IOException {
+		FileWriter fw;
+		try {
+			fw = new FileWriter("highscores.txt", true);
+		} 
+		catch (IOException e) {
+			return;
+		}
+
+		for (int i = 1; i <= 5; i++) {
+			fw.write(i + ".\n");
+		}
+
+		fw.close();
+	}
+
+	public String getString() {
+		scores = "";
+		Scanner reader = null;
+		try {
+			reader = new Scanner(new FileReader("highscores.txt"));
+		}
+		catch(Exception e) {
+			System.out.println("Not found");
+		}
+		
+		while (reader.hasNextLine()) {
+			scores += reader.nextLine();
+			scores += "\n";
+		}
+		
+		reader.close();
+		return scores;
+	}
+	
+	String scores;
+}
