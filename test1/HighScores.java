8612f8a61101bdc1227f94cb629e3c0ecc1a4657
elbuo <yamil.asusta@upr.edu>
2012-04-03 01:44:55 -0400

    Implemented the update method

--- test1/HighScores.java
+++ test1/HighScores.java
@@ -1,8 +1,10 @@
 package test1;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HighScores {
@@ -26,9 +28,65 @@ public class HighScores {
 
 		fw.close();
 	}
-	
-	public void update() {
-		
+
+	public void update(int total, String name) throws IOException {
+		if(!new File("highscores.txt").exists()) {
+			generateFile();
+			FileWriter fw;
+			try {
+				fw = new FileWriter("highscores.txt", true);
+			} 
+			catch (IOException e) {
+				return;
+			}
+			fw.write("1. " + total +", "+ name);
+			fw.close();
+		}
+		else {
+			ArrayList<String> top = new ArrayList<String>();
+			Scanner reader;
+			try
+			{
+				reader = new Scanner(new FileReader("highscores.txt"));
+			}
+			catch(Exception e)
+			{
+				System.out.println("Not found");
+				return;
+			}
+
+			while (reader.hasNextLine()) {
+				scores = reader.nextLine();
+				scores = scores.substring(2);
+				top.add(scores);
+			}
+			reader.close();
+
+			for(int i = 0; i < top.size(); i++) {
+				String[] parse = top.get(i).split(",");
+				if(Integer.parseInt(parse[0]) < total) {
+					top.add(i, total + ", " + name);
+					top.remove(5);
+				}
+			}
+
+			for (int i = 0; i < top.size(); i++) 
+				top.set(i, i+1 + ". " + top.get(i));
+
+			FileWriter fw;
+			try {
+				fw = new FileWriter("mail_listing.txt", true);
+			} 
+			catch (IOException e) {
+				return;
+			}
+			
+			for (int i = 0; i < top.size(); i++) 
+				fw.write(top.get(i));
+			
+			
+			fw.close();
+		}
 	}
 
 	public String getString() {
@@ -40,15 +98,15 @@ public class HighScores {
 		catch(Exception e) {
 			System.out.println("Not found");
 		}
-		
+
 		while (reader.hasNextLine()) {
 			scores += reader.nextLine();
 			scores += "\n";
 		}
-		
+
 		reader.close();
 		return scores;
 	}
-	
-	String scores;
+
+	private String scores;
 }
