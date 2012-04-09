4c3678372c39efe794296b71b72984817d44d93a
Cesar Cruz <cesarcruz91@gmail.com>
2012-04-08 21:38:58 -0400

    Some changes to the Tiles class and The Grid class
    Added rectangle instance variable to each tile object and methods to
    modify this rectangle and other methods. Also added the Grid Class.

--- test1/Tiles.java
+++ test1/Tiles.java
@@ -1,3 +1,5 @@
+import java.awt.Rectangle;
+
 /**
  * 
  */
@@ -13,9 +15,8 @@ public class Tiles {
 	private int locationY = 0;
 	private boolean hasAship = false;
 	private boolean isHit = false;
-	
-	
-	
+	private int boatSerial;
+	private Rectangle rect = new Rectangle();
 	
 	/**
 	 * Each object contains its location in the 2D array that is represented in the grid
@@ -23,10 +24,13 @@ public class Tiles {
 	 * @param x location of the tile in the 2D array 
 	 * @param y location of the tile in the 2D array
 	 */
-	public Tiles(int x, int y){
-		locationX = x;
-		locationY = y;
+	public Tiles(){
+		locationX = 0;
+		locationY = 0;
 		hasAship = false;
+		boatSerial = 0;
+		rect.setBounds(0,0,30,30);
+		isHit = false;
 	}
 	
 	/**
@@ -57,8 +61,47 @@ public class Tiles {
 	public void placeBoat(int x, int y){
 		hasAship = true;
 	}
+	/**
+	 * Stores the location of the tile within the Grid in the respective tile
+	 * @param x
+	 * @param y
+	 */
 	public void addLocation(int x, int y){
 		locationX = x;
 		locationY = y;
+	}
+	/**
+	 * Sets a specific serial to each of the tiles containing a ship.
+	 * @param x
+	 */
+	public void setSerial(int x){
+		boatSerial = x;
+	}
+	
+	/**
+	 * Given the top x and top y coordinates, modifies this values for the rectangle
+	 * in each of the tiles.
+	 * @param topX 
+	 * @param topY
+	 */
+	public void setRectangleBounds(int topX, int topY){
+		rect.setBounds(topX, topY, 30, 30);
+	}
+	
+	/**
+	 * @return the rectangle data for the given tile
+	 */
+	public Rectangle getRect(){
+		return rect;
+	}
+	/**
+	 * 
+	 * @return whether or not the specific tile has been hit or not.
+	 */
+	public boolean isHit(){
+		boolean hitNoHit = false;
+		if(isHit == true)
+			hitNoHit = true;
+		return hitNoHit;
 	}
 }
