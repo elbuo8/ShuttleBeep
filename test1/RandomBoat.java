13f733135b405508faf8f77719132ea49c9bb7c5
Cesar Cruz <cesarcruz91@gmail.com>
2012-04-17 23:23:24 -0400

    Random diagonal, horizontal and vertical methods work… still the one fucking boat bug.

--- test1/RandomBoat.java
+++ test1/RandomBoat.java
@@ -18,14 +18,14 @@ public class RandomBoat {
 		rand = new Random();
 	}
 
-/**
- * Given the number of ships, places random ships in the current Grid at play.
- * @param k Number of ships
- * @param rows Number of Rows in a grid
- * @param columns Number of Columns in a grid
- * @param currentGrid Currently selected grid
- * @param level Currently selected level
- */
+	/**
+	 * Given the number of ships, places random ships in the current Grid at play.
+	 * @param k Number of ships
+	 * @param rows Number of Rows in a grid
+	 * @param columns Number of Columns in a grid
+	 * @param currentGrid Currently selected grid
+	 * @param level Currently selected level
+	 */
 	public void placeRandomBoats(int k, int rows, int columns, TheGrid currentGrid, boolean level){
 		available = new int[k];
 		for(int i = 0; i < k; i++){
@@ -44,9 +44,9 @@ public class RandomBoat {
 					int y1 = rand.nextInt(columns);
 					int y2;
 					do{
-						if(y1 > columns/2){
-							y2 = y1;
-							y1 = y2 - rand.nextInt(k + 2);
+						if(y1 >= columns/2){
+							y1 = y1 - (columns/2);
+							y2 = rand.nextInt(k + 2) + y1;
 						}
 
 						else{
@@ -59,7 +59,8 @@ public class RandomBoat {
 							}
 							check2 = true;
 						}
-					}while(!check2);
+					}while(!check2 && (y2-y1) >= 2);
+
 					boatSerial = (y2 - y1) + 1;
 					currentGrid.setSerial(boatSerial);
 					check1 = currentGrid.addBoatHorizontal(x, y1, y2);
@@ -79,9 +80,9 @@ public class RandomBoat {
 					int x1 = rand.nextInt(rows);
 					int x2;
 					do{
-						if(x1 > rows/2){
-							x2 = x1;
-							x1 = x2 - rand.nextInt(k + 2);
+						if(x1 >= rows/2){
+							x1 = x1 - (rows/2);
+							x2 = rand.nextInt(k + 2) + x1;
 						}
 
 						else{
@@ -94,10 +95,10 @@ public class RandomBoat {
 							}
 							check2 = true;
 						}
-					}while(!check2);
-					
+					}while(!check2 && (x2 - x1) >= 2);
+
 					boatSerial = (x2 - x1) + 1;
-					
+
 					currentGrid.setSerial(boatSerial);
 					check1 = currentGrid.addBoatVertical(y, x1, x2);
 
@@ -126,15 +127,17 @@ public class RandomBoat {
 						}
 					}
 
-					/**
-					 * Esta resta jamas puede dar 0. Primer bug pq boatSerial crece asta 10 y rows por default es 10.
-					 * Luego no puedes dejar q se kede iterando aki. Haz un checkeo, si es un 8 row game el size 10 se alloca primero.
-					 * Problema resuelto.
-					 * 
-					 */
-					
-					int x = rand.nextInt(rows - boatSerial);
-					int	y = rand.nextInt(columns - boatSerial);
+
+					int x;
+					int y;
+					if(rows == boatSerial)
+						x = 0;
+					else
+						x = rand.nextInt(rows - boatSerial);
+					if(columns == boatSerial)
+						y = 0;
+					else
+						y = rand.nextInt(columns - boatSerial);
 
 					currentGrid.setSerial(boatSerial);
 					check1 = currentGrid.addBoatDiagonal(x, y, true);
